db64329d620785bd46153c5595a67144f48a103e
Evan Hackstadt <evan.hackstadt@gmail.com>
2025-02-15 14:07:27 -0800

    Set up skeleton functions

--- content.js
+++ content.js
@@ -1,19 +1,39 @@
-// Method 2 - title tag - TODO LATER as fallback
 /*
-let title_tag = document.querySelector(title_tag);
+NOTES
+THESE ARE SOME PROBLEMATIC BRANDS AND MIGHT NEED EDGE CASE
+Case1 (url subset of name by spaces): Abercrombie & Fitch, Aldi Nord, ALdi South, A
+Case2 (url subset of name, no spaces): BCBGMAXAZRIA
+Case3 (url equal to name, no spaces): Dick's Sporting Goods
+Case3 (url subset of name, special chars): Bloomingdale's
 
-console.log(title_tag);
-console.log(typeof(title_tag));
-
-// format our title tag
-
-title_tag.toLowerCase();
-let words = title_tag.split(" ");
+Approach 2 (url bad, use title tag):American Eagle,
 */
 
+//TODO IMPLEMENT SMARTER LINK PARSING
+/*
+e.g Bananana Republic has www.
+*/
+
+
+// Functions - used to figure out brand name for various cases
+
+function urlBrandDirect(url) {
+
+}
+
+function urlTitleTag(url) {
+
+}
+
+function urlBrandSubset(url) {
+
+}
+
+function urlBrandSpecial(url) {
+
+}
+
 // Method 1 - website URL - TODO NOW
-
-
 
 // array of brand names
 // const brandNames = ...
@@ -27,7 +47,7 @@ let urlByPeriod = url.split(".");
 let brandName = url[1];
 
 let brandNamesLower = brandNames;
-for(let i = 0; i < brandNames.length; i++){
+for (let i = 0; i < brandNames.length; i++) {
     brandNamesLower[i].toLowerCase();
 }
 
@@ -52,18 +72,18 @@ if(brandNames.includes(brandName)) {
 }
 */
 
+// Method 2 - title tag - TODO LATER as fallback
+/*
+let title_tag = document.querySelector(title_tag);
+
+console.log(title_tag);
+console.log(typeof(title_tag));
+
+// format our title tag
+
+title_tag.toLowerCase();
+let words = title_tag.split(" ");
+*/
+
 // pass brandName to background.js if valid
 
-/* THESE ARE SOME PROBLEMATIC BRANDS AND MIGHT NEED EDGE CASE
-Case1 (url subset of name by spaces): Abercrombie & Fitch, Aldi Nord, ALdi South, A
-Case2 (url subset of name, no spaces): BCBGMAXAZRIA
-Case3 (url equal to name, no spaces): Dick's Sporting Goods
-Case3 (url subset of name, special chars): Bloomingdale's
-
-Approach 2 (url bad, use title tag):American Eagle,
-*/
-
-//TODO IMPLEMENT SMARTER LINK PARSING
-/*
-e.g Bananana Republic has www.
-*/
