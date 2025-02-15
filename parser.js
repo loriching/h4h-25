22d7257820ced1e65636dc7359eafb8533f5fbe0
Ryan Deivert <radeivert@gmail.com>
2025-02-15 11:33:11 -0800

    added parser datatype

--- parser.js
+++ parser.js
@@ -1,9 +1,17 @@
+class BrandScore {
+    constructor(name, score) {
+        this.name = name;
+        this.score = score;
+    }
+};
+
 const parse = (filename) => {
     const fs = require("fs");
     fs.readFile(filename, (err, input) => {
         if (err) throw err;
-        console.log(input);
+        console.log(input.toString());
     });
-}
+};
 
+parse("FTC 2023 Final Scores.csv");
 
