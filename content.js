f0f81415d6ca3e17d011d54c149906a0a74846a8
Evan Hackstadt <evan.hackstadt@gmail.com>
2025-02-15 11:59:47 -0800

    Tried and failed reading title tag

--- content.js
+++ content.js
@@ -1,8 +1,18 @@
+// Method 2 - title tag - TODO LATER as fallback
+/*
 let title_tag = document.querySelector(title_tag);
 
 console.log(title_tag);
 console.log(typeof(title_tag));
 
-// array of brand names
+// format our title tag
 
-for ()
+title_tag.toLowerCase();
+let words = title_tag.split(" ");
+*/
+
+// Method 1 - website URL - TODO NOW
+
+
+
+// array of brand names
