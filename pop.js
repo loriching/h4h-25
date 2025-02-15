e341f036b3938b6a4d1a3137c2ff5ca20d488cad
Lori Ching <lching@scu.edu>
2025-02-15 14:49:28 -0800

    Moving stuff to the js file

--- pop.js
+++ pop.js
@@ -1,2 +1,22 @@
-let scoreELement = document.getElementById('myScore')
-scoreELement.innerHTML = "12345";
+let scoreElement = document.getElementById('myScore')
+scoreElement.innerHTML = "12345";
+
+const xValues = [scoreElement, "Not applicable"];
+const yValues = [0.23, 0.77];
+const barColors = [
+    "red",
+    "gray"
+]
+
+newChart("myChart", {
+    type: "doughnutChart",
+    date: {
+    labels: xValues,
+    datasets: [{
+        backgroundColor: donutColors, data: yva
+    }]
+    },
+    options: {
+    title: true, text: "Brand Sustainability Score"
+    }
+});
