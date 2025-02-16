3846ff60ec9f6b28371e3805160a85c7684c5bb9
R-Guglin <rebecca.guglin@gmail.com>
2025-02-15 17:12:51 -0800

    updating python stuff

--- app.py
+++ app.py
@@ -1,44 +1,41 @@
-from flask import Flask, request, jsonify
-from flask_cors import CORS
-from csv import DictReader
+import json
 import string
+from csv import DictReader
 
 fti = {}
 
-with open("fti.csv", mode = 'r', newline = '', encoding = 'utf-8-sig') as file:
-    reader = DictReader(file)
-    for row in reader:
-        brand = row["Brand Name"]
-        score = row["2023 FINAL SCORE"]
-        fti[brand] = score
-
-def lookupBrand(brand):
-    if brand in fti.keys():
-        return fti[brand]
-
-app = Flask(__name__)
-CORS(app)
-
 def parse(text):
+    if text == '':
+        return {
+            'statusCode': 400,
+            'body': json.dumps({'message': 'Text is empty'})
+        }
+
     # Clean up page title
-    text.strip(".")
-    text.strip(",")
-    print(text)
-    # Not super efficient, but it works
+    translator = str.maketrans('', '', ["," , "."])
+    text = text.translate(translator).lower()
+
     for brand in fti.keys():
-        options = [brand, brand.lower(), brand.upper(), brand.title()]
-        for option in options:
-            if option in text:
-                score = fti[option]
-                print(score)
-                return jsonify({"rating": score})
+        if brand in text:
+            score = fti[brand]
+            print(score)
+            return jsonify({"rating": score})
     print("Brand not found")
-    return jsonify({"rating": "0"})
+    return json.dumps({"rating": 0})
 
-@app.route('/lookup', methods = ["POST"])
-def lookup():
-    data = request.get_json()
-    return parse(data.get("text", ""))
+def lambda_handler(event, context):
+    global fti
 
-if __name__ == "__main__":
-    app.run(port=4999, debug = True)
+    if fti == {}:
+        with open('fti.json', 'r') as f:
+            fti = json.load(f)
+            
+    if not event.get('body'):
+        return {
+            'statusCode': 400,
+            'body': json.dumps({'message': 'Request body is missing'})
+        }
+
+    body = json.loads(event['body'])
+    text = body.get('text').strip()
+    return parse(text)
