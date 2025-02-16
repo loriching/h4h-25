c85f0af14916da56e403c5806aaf449a1d5135a0
R-Guglin <147883918+R-Guglin@users.noreply.github.com>
2025-02-15 22:19:37 -0800

    Update app.py

--- app.py
+++ app.py
@@ -1,8 +1,8 @@
 # INSTRUCTIONS FOR RUNNING THIS:
 # If you don't already have the libraries below installed, you'll have to do that
-# (pip install flask and flask-cors and maybe json idk)
 # Then just open a terminal and do python app.py
 # That will launch the server and start listening for POST requests from the Chrome extension
+
 
 from flask import Flask, request, jsonify
 from flask_cors import CORS
@@ -17,20 +17,16 @@ with open('fti.json', 'r') as f:
     fti = json.load(f)
 
 def parse(text):
-    if text == '':
+    if not text:
         return {
             "statusCode": 400,
             "body": json.dumps({"message": "Text is empty"})
         }
 
     # Clean up page title (translator method didn't work)
-    # Issues: s.oliver, ito-yokado (note it's not an issue with the hyphen because k-way, li-ning etc work)
-    cleaned = ""
-    for word in text:
-        cleaned += word.strip(".").strip(",").lower()
-    
-    print("Cleaned webpage title:", cleaned)
-
+    # Issues: s.oliver, dr. martens, ito-yokado (note it's Not an issue with the hyphen because k-way, li-ning etc work)
+    cleaned = " ".join(word.strip(".,:—;").lower() for word in text.split())
+    print(cleaned)
     for brand in fti.keys():
         if brand in cleaned:
             score = fti[brand]
@@ -38,11 +34,16 @@ def parse(text):
             print(score)
             return {
                         "statusCode": 200,
-                        "body": json.dumps({"rating": score})
+                        "body": json.dumps({
+                            "rating": score,
+                            "brand": brand.title()
+                            # Issue here: capitalizes every word
+                            # Possible solution: record the index of the row with this brand in it
+                            })
                    }
     print("Brand not found")
     return {
-                "statusCode": 200,
+                "statusCode": 400,
                 "body": json.dumps({"rating": -999})
             }
 
