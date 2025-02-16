80e7be32aae2a2473fbca774da7f24be84019739
R-Guglin <147883918+R-Guglin@users.noreply.github.com>
2025-02-15 22:55:43 -0800

    Update pop.js

--- pop.js
+++ pop.js
@@ -1,9 +1,10 @@
-chrome.storage.local.get(["rating", "brand"], (data) => {
+chrome.storage.local.get(["rating", "brand", "suggestions"], (data) => {
     let scoreElement = document.getElementById('myScore');
     // Check if the rating and brand are available
-    if (data.rating && data.brand) {
+    if (data.rating && data.brand && data.suggestions) {
         console.log("Stored Rating:", data.rating);
         console.log("Stored Brand:", data.brand);
+        console.log("Stored Suggestions:", data.suggestions);
 
         scoreElement.innerHTML = data.brand;
 
