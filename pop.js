9404e049ebae9c585336cdeb54474646780afdd5
anjiefaith <aobasunloye@gmail.com>
2025-02-15 18:12:14 -0800

    tried to make overlay and failed, changed the circle to be dynamic

--- pop.js
+++ pop.js
@@ -1,14 +1,31 @@
 let scoreElement = document.getElementById('myScore')
 scoreElement.innerHTML = "12345";
+
+let transparencyScore = 0.8;
+let complementTScore = 1 - transparencyScore;
+
+let scoreColor;
+
+if (transparencyScore >= 0.6){
+  scoreColor = 'green'
+}
+else {
+  scoreColor = 'red'
+}
+
 const ctx=document.getElementById('myDonut')
   new Chart(ctx, {
     type: 'doughnut',
     data: {
-      labels: ['Red', 'Blue', 'Yellow', 'Green', 'Purple', 'Orange'],
       datasets: [{
-        label: '# of Votes',
-        data: [12, 19, 3, 5, 2, 3],
+        label: 'Transperancy Index',
+        data: [complementTScore, transparencyScore],
+        backgroundColor: ['gray', scoreColor]
         
       }]
+    }, 
+    options: {
+      cutout: "80%",
+      radius: 140
     }
-  });
+});
