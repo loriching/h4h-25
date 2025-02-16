1361996dee27bcf272b89760e2a651c3502d4e9a
anjiefaith <aobasunloye@gmail.com>
2025-02-15 23:34:42 -0800

    changed shadow and pushed down

--- pop.js
+++ pop.js
@@ -1,6 +1,6 @@
 // Inject brand name and score to HTML
 let scoreElement = document.getElementById('brand-score')
-scoreElement.innerHTML = 50 // GET SCORE HERE
+scoreElement.innerHTML = 100 // GET SCORE HERE
 let nameElement = document.getElementById('brand-name')
 nameElement.innerHTML = "No Data on this Brand"
 
@@ -9,11 +9,15 @@ let maxScore = 207.5;
 let brandScore = document.getElementById('brand-score').innerHTML;
 let diffScore = maxScore - brandScore;
 
-
+//0-40, 40-70,70-100
 let scoreColor;
-if (brandScore >= 100) {
+if (brandScore >= 145.25) {
   scoreColor = 'green'
-} else {
+} 
+else if(brandScore >=82.88){
+  scoreColor = 'orange'
+}
+else {
   scoreColor = 'red'
 }
 
