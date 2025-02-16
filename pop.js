a8a1f0df1d6536b0bce3888e61c93bd21783bdbb
anjiefaith <aobasunloye@gmail.com>
2025-02-15 17:11:44 -0800

    Edit popup files

--- pop.js
+++ pop.js
@@ -1,44 +1,14 @@
 let scoreElement = document.getElementById('myScore')
 scoreElement.innerHTML = "12345";
 const ctx=document.getElementById('myDonut')
-/*const data = {
-    labels: [
-      'Red',
-      'Blue',
-      'Yellow'
-    ],
-    datasets: [{
-      label: 'My First Dataset',
-      data: [300, 50, 100],
-      backgroundColor: [
-        'rgb(255, 99, 132)',
-        'rgb(54, 162, 235)',
-        'rgb(255, 205, 86)'
-      ],
-      hoverOffset: 4
-    }]
-  };
-  const config={
-    type: 'doughnut',
-    data:data
-  };
-
-  const myDonut= new Chart(ctx, config);*/
   new Chart(ctx, {
-    type: 'bar',
+    type: 'doughnut',
     data: {
       labels: ['Red', 'Blue', 'Yellow', 'Green', 'Purple', 'Orange'],
       datasets: [{
         label: '# of Votes',
         data: [12, 19, 3, 5, 2, 3],
-        borderWidth: 1
+        
       }]
-    },
-    options: {
-      scales: {
-        y: {
-          beginAtZero: true
-        }
-      }
     }
   });
