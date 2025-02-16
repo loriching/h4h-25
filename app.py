c2883f4db8d89f90e01234cc506f093db8d0b614
R-Guglin <147883918+R-Guglin@users.noreply.github.com>
2025-02-15 23:24:33 -0800

    Update app.py

--- app.py
+++ app.py
@@ -2,6 +2,8 @@
 # If you don't already have the libraries below installed, you'll have to do that
 # Then just open a terminal and do python app.py
 # That will launch the server and start listening for POST requests from the Chrome extension
+
+
 from flask import Flask, request, jsonify
 from flask_cors import CORS
 import string
@@ -37,10 +39,12 @@ def parse(text):
             "body": json.dumps({"message": "Text is empty"})
         }
 
+    cleaned = []
     # Clean up page title (translator method didn't work)
     # Issues: s.oliver, dr. martens, ito-yokado (note it's Not an issue with the hyphen because k-way, li-ning etc work)
-    cleaned = " ".join(word.strip(".,:—;").lower() for word in text.split())
-    print(cleaned)
+    for word in text.split():
+        cleaned.append(word.strip(".,:—;®™").lower())
+    print(cleaned[0])
     for brand in fti.keys():
         if brand in cleaned:
             score = fti[brand]
@@ -49,19 +53,21 @@ def parse(text):
             suggestions = suggestAlternatives(brand)
             print("Suggestions:", suggestions)
             return {
-                "statusCode": 200,
-                "body": json.dumps({
-                    "rating": score,
-                    "brand": brand.title(),
-                    "suggestions": suggestions[:3],
-                })
-            }
+                        "statusCode": 200,
+                        "body": json.dumps(
+                            {
+                                "rating": score,
+                                "brand": brand.title(),
+                                "suggestions": suggestions[:3]
+                            }
+                        )
+                   }
 
     print("Brand not found")
     return {
-        "statusCode": 400,
-        "body": json.dumps({"rating": -999})
-    }
+                "statusCode": 400,
+                "body": json.dumps({"rating": -999})
+            }
 
 @app.route('/lookup', methods = ["POST"])
 def lookup():
@@ -69,5 +75,4 @@ def lookup():
     return parse(data.get("text", ""))
     
 if __name__ == "__main__":
-    app.run(port=5000, debug=True)
-
+    app.run(port=5000, debug = True)
