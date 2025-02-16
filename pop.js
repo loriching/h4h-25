fecec7c77c927cc96895d54dbf987e54a61966fe
Evan Hackstadt <evan.hackstadt@gmail.com>
2025-02-16 00:03:06 -0800

    Fix pop.js brand scores

--- pop.js
+++ pop.js
@@ -1,33 +1,34 @@
-chrome.storage.local.get(["rating", "brand", "suggestions"], (data) => {
-    // Check if the rating and brand are available
-    if (data.rating && data.brand && data.suggestions) {
-        console.log("Stored Rating:", data.rating);
-        console.log("Stored Brand:", data.brand);
-        console.log("Stored Suggestions:", data.suggestions);
-
-        // inject into HTML
-        let scoreElement = document.getElementById('brand-score');
-        let nameElement = document.getElementById('brand-name');
-        let suggestionElement1 = document.getElementById('suggestion1');
-        let suggestionElement2 = document.getElementById('suggestion2');
-        let suggestionElement3 = document.getElementById('suggestion3');
-        scoreElement.innerHTML = data.rating;
-        nameElement.innerHTML = data.brand;
-        suggestionElement1.innerHTML = data.suggestions[0].name;
-        suggestionElement2.innerHTML = data.suggestions[1].name;
-        suggestionElement3.innerHTML = data.suggestions[2].name;
-
-    } else {
-        console.log("No data found in storage.");
-        let errorMsgElement = document.getElementById("error-msg");
-        errorMsgElement.setAttribute("style", "visibility: visible");
-    }
-});
-
-// Score calculations
+// score calculations
 let maxScore = 207.5;
-let brandScore = document.getElementById('brand-score').innerHTML;
+let brandScore;
 let diffScore = maxScore - brandScore;
+
+chrome.storage.local.get(["rating", "brand", "suggestions"], (data) => {
+// Check if the rating and brand are available
+  if (data.rating && data.brand && data.suggestions) {
+      console.log("Stored Rating:", data.rating);
+      console.log("Stored Brand:", data.brand);
+      console.log("Stored Suggestions:", data.suggestions);
+
+      // inject into HTML
+      let scoreElement = document.getElementById('brand-score');
+      let nameElement = document.getElementById('brand-name');
+      let suggestionElement1 = document.getElementById('suggestion1');
+      let suggestionElement2 = document.getElementById('suggestion2');
+      let suggestionElement3 = document.getElementById('suggestion3');
+      scoreElement.innerHTML = data.rating;
+      brandScore = data.rating;
+      nameElement.innerHTML = data.brand;
+      suggestionElement1.innerHTML = data.suggestions[0].name;
+      suggestionElement2.innerHTML = data.suggestions[1].name;
+      suggestionElement3.innerHTML = data.suggestions[2].name;
+
+  } else {
+      console.log("No data found in storage.");
+      let errorMsgElement = document.getElementById("error-msg");
+      errorMsgElement.setAttribute("style", "visibility: visible");
+  }
+});
 
 //0-40, 40-70,70-100
 let scoreColor;
